283ba2e17fbf9c9d22e668ee50a270dc5c258244
Jose Luis Burgos Hernandez <jose_burgos3@ucne.edu.do>
2020-07-06 19:12:08 -0400

    - Programando la clase contexto (OnConfiguring y OnModelCreating)

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,25 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
 using Microsoft.EntityFrameworkCore;
-
+using SegundoParcial_JoseLuis.Entidades;
 
 namespace SegundoParcial_JoseLuis.DAL
 {
     public class Contexto : DbContext
     {
-
+        public DbSet<Proyectos> Jugadores { get; set; }
+        public DbSet<Tareas> Partidas { get; set; }
 
         //——————————————————————————————————————————————————————————————————————————————————————
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
+            optionsBuilder.UseSqlite(@"Data Source= Data\ProyectoTareas_DB.db");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<Tareas>().HasData(new Tareas { TareaId = 1, TipoTarea = "Analisis", Requerimiento = "Analizar la opción de clientes", Tiempo = 120 });
+            modelBuilder.Entity<Tareas>().HasData(new Tareas { TareaId = 2, TipoTarea = "Diseño", Requerimiento = "Hacer un diseño excelente", Tiempo = 60 });
+            modelBuilder.Entity<Tareas>().HasData(new Tareas { TareaId = 3, TipoTarea = "Programación", Requerimiento = "Programar todo el registro", Tiempo = 240 });
+            modelBuilder.Entity<Tareas>().HasData(new Tareas { TareaId = 4, TipoTarea = "Prueba", Requerimiento = "Probar con mucho cuidado", Tiempo = 30 });
         }
     }
 }
