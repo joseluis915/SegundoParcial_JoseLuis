06de4f4bbea89964d6d9ebe71bc482f05b4b2dc4
Jose Luis Burgos Hernandez <jose_burgos3@ucne.edu.do>
2020-07-06 20:01:40 -0400

    - Empezando a programar  la clase rProyectos.xaml.cs (Valores del ComboBox TipoTarea, Cargar, Limpiar y Validar)

--- UI/Registros/rProyectos.xaml.cs
+++ UI/Registros/rProyectos.xaml.cs
@@ -9,49 +9,79 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
 using SegundoParcial_JoseLuis.BLL;
-
+using SegundoParcial_JoseLuis.Entidades;
 
 namespace SegundoParcial_JoseLuis.UI.Registros
 {
     public partial class rProyectos : Window
     {
+        private Proyectos proyectos = new Proyectos();
         public rProyectos()
         {
             InitializeComponent();
-        }
+            this.DataContext = proyectos;
 
+            //—————————————————————————————————————[ VALORES DEL ComboBox ]—————————————————————————————————————
+            JugadoresComboBox.SelectedValuePath = "TareasId";
+            JugadoresComboBox.DisplayMemberPath = "TipoTarea";
+            JugadoresComboBox.ItemsSource = TareasBLL.GetList();
+        }
+        //——————————————————————————————————————————————————————————————[ Cargar ]———————————————————————————————————————————————————————————————
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = proyectos;
+        }
+        //——————————————————————————————————————————————————————————————[ Limpiar ]——————————————————————————————————————————————————————————————
+        private void Limpiar()
+        {
+            this.proyectos = new Proyectos();
+            this.DataContext = proyectos;
+        }
+        //——————————————————————————————————————————————————————————————[ Validar ]——————————————————————————————————————————————————————————————
+        private bool Validar()
+        {
+            bool Validado = true;
+            if (ProyectoIdTextbox.Text.Length == 0)
+            {
+                Validado = false;
+                MessageBox.Show("Transaccion Fallida", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            return Validado;
+        }
+        //——————————————————————————————————————————————————————————————[ Buscar ]———————————————————————————————————————————————————————————————
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
-
+        //——————————————————————————————————————————————————————————————[ Agregar Fila ]———————————————————————————————————————————————————————————————
         private void AgregarFilaButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
-
+        //——————————————————————————————————————————————————————————————[ Remover Fila ]———————————————————————————————————————————————————————————————
         private void RemoverFilaButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
-
+        //——————————————————————————————————————————————————————————————[ Nuevo ]———————————————————————————————————————————————————————————————
         private void NuevoButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
-
+        //——————————————————————————————————————————————————————————————[ Guardar ]———————————————————————————————————————————————————————————————
         private void GuardarButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
-
+        //——————————————————————————————————————————————————————————————[ Eliminar ]———————————————————————————————————————————————————————————————
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
-
+        //——————————————————————————————————————————————————————————————[ Tiempo Total ]———————————————————————————————————————————————————————————————
         private void TiempoTotalTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
